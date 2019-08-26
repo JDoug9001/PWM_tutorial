96759f765972ebcc324083fa47d87357aa46517c
Joshua Douglas <jdouglas@Rivendale.local>
2019-08-26 14:48:31 -0400

    completed pwm with fade and brighten

--- PWM Tutorial .ino
+++ PWM Tutorial .ino
@@ -1,7 +1,6 @@
 //PWM Tutorial 
-//Understood. complete
+/*
   Button
-//testing testing one two three
   Turns on and off a light emitting diode(LED) connected to digital pin 13,
   when pressing a pushbutton attached to pin 2.
 
@@ -10,17 +9,6 @@
   - pushbutton attached to pin 2 from +5V
   - 10K resistor attached to pin 2 from ground
 
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Button
 */
 
 // constants won't change. They're used here to set pin numbers:
@@ -29,6 +17,8 @@ const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
+int Counter = 1;
+
 
 void setup() {
   // initialize the LED pin as an output:
@@ -43,8 +33,14 @@ void loop() {
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    analogWrite(ledPin, 64);
+  Counter++ ; //Increment Counter   
+    if (Counter>3){
+    Counter=1;
+    }
+
+switch (Counter) {
+  case 1 :
+     analogWrite(ledPin, 64);
     delay (125);
     analogWrite(ledPin, 127);
     delay (125);
@@ -80,8 +76,31 @@ void loop() {
     delay (500);
     analogWrite(ledPin, 255);
     delay (500);
-  } else {
-    // turn LED off:
-    analogWrite(ledPin, 0);
+    break;
+
+  case 2 :
+    for(int value = 0; value<=255; value++){
+    analogWrite(ledPin, value);
+    delay(10);
+    for (value = 255){
+      value = 0;
+    }
+    }
+    break;
+    
+    case 3 :
+    for(int value = 255; value>=0; value--){
+    analogWrite(ledPin, value);
+    delay(10);
+    for (value = 0){
+      value = 255;
+    } 
+    break;
+
+  default:
+   analogWrite(ledPin, 0); // statements
+    break;
+}
+}
   }
 }
